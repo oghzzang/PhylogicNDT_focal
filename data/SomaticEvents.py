14178844a2668e58b237011b06491716212a9520
Hayley <33537478+oghzzang@users.noreply.github.com>
2024-08-28 09:48:24 +0900

    Update SomaticEvents.py
    Add the pseudo chromosome

--- data/SomaticEvents.py
+++ data/SomaticEvents.py
@@ -449,20 +449,21 @@ class CN_SegProfile:
     # csize contains chromosome bp lengths
     CSIZE = [0, 249250621, 243199373, 198022430, 191154276, 180915260, 171115067, 159138663, 146364022, 141213431,
              135534747, 135006516, 133851895, 115169878, 107349540, 102531392, 90354753, 81195210, 78077248, 59128983,
-             63025520, 48129895, 51304566, 156040895, 57227415]
+             63025520, 48129895, 51304566, 156040895, 57227415, 156500000]
 
     # centromeres (define arm-level lengths)
     CENT_LOOKUP = {1: 125000000, 2: 93300000, 3: 91000000, 4: 50400000, 5: 48400000,
                    6: 61000000, 7: 59900000, 8: 45600000, 9: 49000000, 10: 40200000,
                    11: 53700000, 12: 35800000, 13: 17900000, 14: 17600000, 15: 19000000,
                    16: 36600000, 17: 24000000, 18: 17200000, 19: 26500000, 20: 27500000, 21: 13200000,
-                   22: 14700000, 23: 60600000, 24: 12500000}
+                   22: 14700000, 23: 60600000, 24: 12500000, 25: 155000000}
 
     def __init__(self, seg_file, input_type='auto', from_sample=None):
         # try: # validate correct input
         self.chroms = list(map(str, range(1, 23)))
         self.chroms.append('X')
         self.chroms.append('Y')
+        self.chroms.append('25')
         self.from_sample = from_sample  # from_sample can be an Object, and potential to get purity from this
         logging.debug('reading in :' + seg_file)
         if input_type == 'auto':
